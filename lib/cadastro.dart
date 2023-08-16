7a0411c9b2348b0f554416b304c4c21f250e7992
99128patrick <127446550+99128patrick@users.noreply.github.com>
2023-08-16 17:33:26 -0400

    16/08

--- lib/cadastro.dart
+++ lib/cadastro.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 
 class Cadastro extends StatelessWidget {
@@ -12,20 +14,51 @@ class Cadastro extends StatelessWidget {
       body: ListView(
         padding: EdgeInsets.all(16),
         children: [
+          SizedBox(height: 20),
           TextFormField(
             decoration: InputDecoration(
-              icon: Icon(Icons.email),
-              hintText: "Informe seu email",
+              prefixIcon: Icon(Icons.person),
+              label: Text("Informe seu Nome"),
+              border: OutlineInputBorder(),
             ),
             cursorColor: Color.fromARGB(255, 28, 180, 149),
           ),
+          SizedBox(height: 20),
           TextFormField(
             decoration: InputDecoration(
-              icon: Icon(Icons.lock),
-              hintText: "Informe a senha",
+              prefixIcon: Icon(Icons.email),
+              label: Text("Informe seu Email"),
+              border: OutlineInputBorder(),
             ),
+            cursorColor: Color.fromARGB(255, 28, 180, 149),
           ),
-          SizedBox(height: 30),
+          SizedBox(height: 20),
+          TextFormField(
+            decoration: InputDecoration(
+              prefixIcon: Icon(Icons.email),
+              label: Text("Informe sua senha"),
+              border: OutlineInputBorder(),
+            ),
+            cursorColor: Color.fromARGB(255, 28, 180, 149),
+          ),
+          SizedBox(height: 20),
+          TextFormField(
+            decoration: InputDecoration(
+              prefixIcon: Icon(Icons.today),
+              label: Text("Informe sua data de nascimento"),
+              border: OutlineInputBorder(),
+            ),
+            cursorColor: Color.fromARGB(255, 28, 180, 149),
+            onTap: () {
+              showDatePicker(
+                context: context,
+                initialDate: DateTime.now(),
+                firstDate: DateTime(1900, 1, 1),
+                lastDate: DateTime.now(),
+              );
+            },
+          ),
+          SizedBox(height: 20),
           ElevatedButton(
             onPressed: () {},
             style: ElevatedButton.styleFrom(
